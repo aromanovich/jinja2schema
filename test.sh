ebff14c11b55a79781c84b659428693072d40b29
Anton Romanovich <anthony.romanovich@gmail.com>
2014-06-14 13:44:14 +0600

    introduce pytest-cov

--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-PYTHONPATH=.:$PYTHONPATH py.test ./tests -s --tb=short --showlocals "$@"
+PYTHONPATH=.:$PYTHONPATH py.test -s --tb=short --showlocals \
+                                 --cov-report term-missing --cov jinja2schema ./tests "$@"
