5485548309f6bc0350254e4f380932707cd5f737
Anton Romanovich <anthony.romanovich@gmail.com>
2014-10-11 23:57:29 +0600

    bump version to 0.1.0

--- jinja2schema/__init__.py
+++ jinja2schema/__init__.py
@@ -17,7 +17,7 @@ __title__ = 'jinja2schema'
 __author__ = 'Anton Romanovich'
 __license__ = 'BSD'
 __copyright__ = 'Copyright 2014 Anton Romanovich'
-__version__ = '0.0.12'
+__version__ = '0.1.0'
 __version_info__ = tuple(int(i) for i in __version__.split('.'))
 
 
