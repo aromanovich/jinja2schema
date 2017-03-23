8a39d7487c567337080671666a4d02758d43adec
Anton Romanovich <anthony.romanovich@gmail.com>
2017-03-23 23:57:57 +0500

    Bump version to 0.1.3

--- jinja2schema/__init__.py
+++ jinja2schema/__init__.py
@@ -8,7 +8,7 @@ Type inference for Jinja2 templates.
 
 See http://jinja2schema.rtfd.org/ for documentation.
 
-:copyright: (c) 2014 Anton Romanovich
+:copyright: (c) 2017 Anton Romanovich
 :license: BSD
 
 """
@@ -16,8 +16,8 @@ See http://jinja2schema.rtfd.org/ for documentation.
 __title__ = 'jinja2schema'
 __author__ = 'Anton Romanovich'
 __license__ = 'BSD'
-__copyright__ = 'Copyright 2014 Anton Romanovich'
-__version__ = '0.1.2'
+__copyright__ = 'Copyright 2017 Anton Romanovich'
+__version__ = '0.1.3'
 __version_info__ = tuple(int(i) for i in __version__.split('.'))
 
 
